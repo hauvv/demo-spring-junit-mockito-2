e779d55856a512bca9722f3a397372f377960210
hauvv <vanhau88@gmail.com>
2019-02-14 15:18:43 +0700

    init

---
+++ src/main/java/com/example/demospringjunitmockito/DemoSpringJunitMockitoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demospringjunitmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJunitMockitoApplication.class, args);
+	}
+
+}
+
